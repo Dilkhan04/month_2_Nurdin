be6eca2f7feb439ef9f2d8f377761a7ec14dfe72
Dilkhan04 <dilkhan.04.e@gmail.com>
2022-11-12 01:01:55 +0600

    'dz'

---
+++ dz_3/main.js
@@ -0,0 +1,41 @@
+// Задание №1
+
+// let numbers = {
+//     number1: prompt("Введите первое число?"),
+//     number2: prompt("Введите первое число?")
+// }
+// function numbersMin(number1, number2){
+//     console.log(Math.min(number1, number2))
+// }
+// numbersMin(numbers.number1, numbers.number2)
+
+// Задание №2
+
+// function argumentLength(){
+//     const argument = prompt("Введите что-то?");
+//     console.log(argument.length)
+// }
+// argumentLength()
+
+// Задание №3
+
+// const num1 = prompt("Введите первое число?");
+// const maths = prompt("+ or - or : or * ???");
+// const num2 = prompt("Введите второе число?");
+// let sum;
+// function math(){
+//     if(maths === "+"){
+//         sum = parseInt(num1) + parseInt(num2);
+//         console.log(sum)
+//     } else if(maths === "-"){
+//         sum = num1 - num2;
+//         console.log(sum)
+//     } else if(maths === ":"){
+//         sum = num1 / num2;
+//         console.log(sum)
+//     } else if(maths === "*"){
+//         sum = num1 * num2;
+//         console.log(sum)
+//     }
+// }
+// math()
